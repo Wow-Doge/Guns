25505371280e5065062ec4406945f73c596371dd
Wow-Doge <40496666+Wow-Doge@users.noreply.github.com>
2020-08-18 09:15:55 +0700

    basic

--- Guns/Assets/Scripts/PlayerController.cs
+++ Guns/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+
+    Rigidbody2D rb2d;
+    Vector2 movement;
+    Vector2 mousePos;
+
+    public Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement * speed * Time.fixedDeltaTime);
+        Vector2 lookDirection = mousePos - rb2d.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb2d.rotation = angle;
     }
 }
