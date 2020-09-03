dac550f3e393b1ea5fa6875e8e5109727d042de9
Wow-Doge <40496666+Wow-Doge@users.noreply.github.com>
2020-09-03 14:47:55 +0700

    add GameManager
    keep GameManager when changing scene

---
+++ Guns/Assets/Scripts/Test/InstanceScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceScript : MonoBehaviour
+{
+    private static InstanceScript _instance;
+
+    public static InstanceScript Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+}
